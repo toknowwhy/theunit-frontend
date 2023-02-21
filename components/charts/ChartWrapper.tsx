4329c483d6c04110a937193933b87d415349c679
ShuaJJ <Sody008@gmail.com>
2023-02-21 20:26:37 +0800

    use function instead pure component of trading view

--- components/charts/ChartWrapper.tsx
+++ components/charts/ChartWrapper.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { TVChartContainer } from './TradingView';
+import TVChartContainer from './TradingView';
 
 export default function ChartWrapper({locale} : {locale: string}) {
     return <TVChartContainer />
--- components/charts/TradingView.tsx
+++ components/charts/TradingView.tsx
@@ -1,55 +1,56 @@
-import * as React from 'react';
-import { widget, ChartingLibraryWidgetOptions, IChartingLibraryWidget, ResolutionString, LanguageCode } from '@/public/charting_library';
+import { memo, useEffect, useRef } from 'react';
+import { 
+	widget, 
+	IChartingLibraryWidget, 
+	ResolutionString, 
+	LanguageCode, 
+	AvailableSaveloadVersions, 
+	ThemeName 
+} from '@/public/charting_library';
 import { UnitDatafeed } from './datafeed';
 
-export class TVChartContainer extends React.PureComponent {
-	static defaultProps: { symbol: string, locale: string } = {
-		symbol: 'UNITSATOSHI',
-		locale: 'en',
+
+const TVChartContainer = memo(function TVChartContainer({
+	symbol= 'UNITSATOSHI',
+	locale= 'en',
+} : { 
+	symbol?: string, 
+	locale?: string 
+}) {
+	const ref = useRef(null);
+
+	const widgetOptions = {
+		symbol: symbol,
+		// BEWARE: no trailing slash is expected in feed URL
+		datafeed: new UnitDatafeed(),
+		interval: 'D' as ResolutionString,
+		library_path: '/charting_library/',
+		theme: 'Dark' as ThemeName,
+		locale: locale as LanguageCode,
+		disabled_features: ['use_localstorage_for_settings'],
+		enabled_features: ['study_templates'],
+		charts_storage_url: 'https://saveload.tradingview.com',
+		charts_storage_api_version: '1.1' as AvailableSaveloadVersions,
+		client_id: 'tradingview.com',
+		user_id: 'public_user_id',
+		fullscreen: false,
+		autosize: true,
+		studies_overrides: {},
 	};
 
-	tvWidget: IChartingLibraryWidget | null = null;
-	ref: React.LegacyRef<HTMLDivElement> | null = null;
+	useEffect(() => {
+		const options = {
+			...widgetOptions,
+			container: ref!.current!,
+		}
 
-	constructor(props: { symbol: string, locale: string }) {
-		super(props);
-
-		this.ref = React.createRef<HTMLDivElement>();
-	}
-
-	componentDidMount() {
-
-		const containerRef = this.ref as React.LegacyRef<HTMLDivElement>;
-
-		const widgetOptions: ChartingLibraryWidgetOptions = {
-			symbol: (this.props as { symbol: string, locale: string }).symbol,
-			// BEWARE: no trailing slash is expected in feed URL
-			datafeed: new UnitDatafeed(),
-			interval: 'D' as ResolutionString,
-			container: containerRef!.current,
-			library_path: '/charting_library/',
-			theme: 'Dark',
-			locale: (this.props as { symbol: string, locale: string }).locale as LanguageCode,
-			disabled_features: ['use_localstorage_for_settings'],
-			enabled_features: ['study_templates'],
-			charts_storage_url: 'https://saveload.tradingview.com',
-			charts_storage_api_version: '1.1',
-			client_id: 'tradingview.com',
-			user_id: 'public_user_id',
-			fullscreen: false,
-			autosize: true,
-			studies_overrides: {},
-		};
-
-		const tvWidget = new widget(widgetOptions);
-		this.tvWidget = tvWidget;
-
+		let tvWidget: null | IChartingLibraryWidget = new widget(options);
 		tvWidget.onChartReady(() => {
-			tvWidget.headerReady().then(() => {
-				const button = tvWidget.createButton();
+			tvWidget!.headerReady().then(() => {
+				const button = tvWidget!.createButton();
 				button.setAttribute('title', 'Click to show a notification popup');
 				button.classList.add('apply-common-tooltip');
-				button.addEventListener('click', () => tvWidget.showNoticeDialog({
+				button.addEventListener('click', () => tvWidget!.showNoticeDialog({
 					title: 'Notification',
 					body: 'TradingView Charting Library API works correctly',
 					callback: () => {
@@ -60,20 +61,17 @@ export class TVChartContainer extends React.PureComponent {
 				button.innerHTML = 'Check API';
 			});
 		});
-	}
 
-	componentWillUnmount() {
-		if (this.tvWidget !== null) {
-			this.tvWidget.remove();
-			this.tvWidget = null;
+		return () => {
+			if (tvWidget != null) {
+				tvWidget.remove();
+				tvWidget = null;
+			}
 		}
-	}
 
-	render() {
-		return (	
-			<div className="h-[660px]" ref={(element) => {
-				(this.ref as React.LegacyRef<HTMLDivElement>).current = element;
-			  }} />
-		);
-	}
-}
+	})
+
+	return <div className="h-[660px]" ref={ref}></div>
+})
+
+export default TVChartContainer;
