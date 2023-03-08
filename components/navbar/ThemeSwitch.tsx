7a8405cfe7c5b0b150d40d146696f37e98078d17
ShuaJJ <Sody008@gmail.com>
2023-03-08 12:21:44 +0800

    added a theme switcher

--- components/navbar/ThemeSwitch.tsx
+++ components/navbar/ThemeSwitch.tsx
@@ -1,15 +1,27 @@
 'use client';
 
 import { useTheme } from 'next-themes'
+import Image from 'next/image';
+import light from '@/public/icons/light.png';
+import dark from '@/public/icons/dark.png';
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme()
+  const isDark = theme === 'dark';
+  const changeTheme = () => {
+    if (isDark) {
+        setTheme('light')
+    } else {
+        setTheme('dark')
+    }
+  }
 
-  return (
-    <div>
-      The current theme is: {theme}
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
-    </div>
-  )
+  return <Image 
+            className="cursor-pointer"
+            src={isDark ? dark : light} 
+            alt="theme" 
+            width={32}
+            height={32}
+            onClick={changeTheme} 
+        />
 }
