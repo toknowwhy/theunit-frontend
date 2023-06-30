7b287ec64dad77a12988d999bb8590bb3eac50d8
ShuaJJ <Sody008@gmail.com>
2023-06-30 20:49:15 +0800

    fixed mobile menu color issue

--- components/navbar/MobileMenu.tsx
+++ components/navbar/MobileMenu.tsx
@@ -15,7 +15,7 @@ export default function MobileMenu({children} : {children: ReactNode}) {
       }, [pathname])
 
     return <div className={`group ${active ? 'is-active' : ''}`}>
-        <Hamburger size={23} color={theme === 'dark' ? '#ffffff' : '#000000'} toggled={active} toggle={setActive} />
+        <Hamburger size={23} color={(!theme || theme === 'dark') ? '#ffffff' : '#000000'} toggled={active} toggle={setActive} />
         <div className="hidden fixed right-0 bottom-0 top-16 h-screen bg-black-light group-[.is-active]:block">
             {children}
         </div>
