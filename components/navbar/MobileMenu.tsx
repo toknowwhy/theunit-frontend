d8d102b3a88fa52ef1d073109708dbce9b264788
ShuaJJ <Sody008@gmail.com>
2023-11-10 17:13:14 +0800

    fixed mobile menu hamburger theme issue

--- components/navbar/MobileMenu.tsx
+++ components/navbar/MobileMenu.tsx
@@ -7,15 +7,20 @@ import { useTheme } from "next-themes";
 
 export default function MobileMenu({children} : {children: ReactNode}) {
     const [active, setActive] = useState(false);
+    const [color, setColor] = useState('#ffffff');
     const pathname = usePathname();
     const { theme } = useTheme();
 
     useEffect(() => {
         setActive(false)
-      }, [pathname])
+    }, [pathname])
+
+    useEffect(() => {
+        setColor((!theme || theme === 'dark') ? '#ffffff' : '#000000')
+    }, [theme])  
 
     return <div className={`group ${active ? 'is-active' : ''}`}>
-        <Hamburger size={23} color={(!theme || theme === 'dark') ? '#ffffff' : '#000000'} toggled={active} toggle={setActive} />
+        <Hamburger size={23} color={color} toggled={active} toggle={setActive} />
         <div className="hidden fixed right-0 bottom-0 top-16 h-screen bg-black-light group-[.is-active]:block">
             {children}
         </div>
